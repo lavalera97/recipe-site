8724b76ddf9024325899b4e08738423b79b32484
Valery <fenixor0007@gmail.com>
2021-10-28 20:55:48 +0300

    Made final look of front page

--- cool_recipes/static/js/search-appear.js
+++ cool_recipes/static/js/search-appear.js
@@ -1,8 +1,23 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop()> 40) {
-        $('.search-form').css('display', 'block');
-     }
-    else {
-      $('.search-form').css('display', 'none');
-     }
- });
+$(document).ready(function () {
+
+    ChangeSearch();
+
+    $(window).resize(function () {
+        ChangeSearch();
+    });
+
+    function ChangeSearch() {
+        var bodyWidth = $('body').width();
+        $(window).scroll(function() {
+            if (bodyWidth <= 700) {
+                $('.search-form').css('display', 'none');
+             }
+            else if ($(this).scrollTop()> 40) {
+                $('.search-form').css('display', 'block');
+             }
+            else {
+              $('.search-form').css('display', 'none');
+             }
+         });
+        }
+});
